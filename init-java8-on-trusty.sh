0e8f34b13a9bbbdc02187b9f9302d9a327fd8c07
Gerd Aschemann <gerd@aschemann.net>
2015-06-06 09:41:49 +0200

    Bugfix: Only add Oracle keys if not already loaded

--- init-java8-on-trusty.sh
+++ init-java8-on-trusty.sh
@@ -8,8 +8,11 @@ test -x $sudo || sudo=
 $sudo apt-get -y -q update
 $sudo apt-get -y -q upgrade
 $sudo apt-get -y -q install software-properties-common htop
-$sudo add-apt-repository ppa:webupd8team/java
-$sudo apt-get -y -q update
+if ! test -r /etc/apt/sources.list.d/webupd8team-java-trusty.list; then
+	$sudo add-apt-repository ppa:webupd8team/java
+	$sudo apt-get -y -q update
+	$sudo etckeeper commit 'Added Oracle Java 8 repository'
+fi
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | $sudo /usr/bin/debconf-set-selections
 $sudo apt-get -y -q install oracle-java8-installer
 $sudo update-java-alternatives -s java-8-oracle
