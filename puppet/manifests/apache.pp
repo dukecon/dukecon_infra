4a5d1f7701997615168ea7496a677fad588d3610
Gerd Aschemann <gerd@aschemann.net>
2015-08-29 16:19:33 +0200

    Added Context Path for "latest" container

--- puppet/manifests/apache.pp
+++ puppet/manifests/apache.pp
@@ -12,6 +12,7 @@ apache::vhost { 'dev.dukecon.org':
   port			=>	'80',
   docroot		=>	'/var/www/html',
   allow_encoded_slashes	=>	'nodecode',
+  proxy_preserve_host	=>	'true',
   proxy_pass		=>	[
     {	'path'		=>	'/jenkins',
 	'url'		=>	'http://localhost:8080/jenkins',
@@ -21,7 +22,7 @@ apache::vhost { 'dev.dukecon.org':
 	'url'		=>	'http://localhost:8081/',
     },
     {	'path'		=>	'/latest/',
-	'url'		=>	'http://localhost:9050/',
+	'url'		=>	'http://localhost:9050/latest/',
     },
     {	'path'		=>	'/testing/',
 	'url'		=>	'http://localhost:9060/',
--- puppet/manifests/docker-dukecon-latest.pp
+++ puppet/manifests/docker-dukecon-latest.pp
@@ -1,4 +1,5 @@
 docker::run { 'dukecon-latest':
   image		=> 'ascheman/dukecon-server',
   ports		=> ['9050:8080'],
+  env		=> ['SPRING_PROFILES_ACTIVE=latest'],
 }
