225895bfdbafbb956675894c85f493438f6948f9
Gerd Aschemann <gerd@aschemann.net>
2015-09-21 07:32:23 +0200

    Allow for unauthorized (test) access to preferences

--- puppet/manifests/docker-dukecon-latest.pp
+++ puppet/manifests/docker-dukecon-latest.pp
@@ -1,5 +1,5 @@
 docker::run { 'dukecon-latest':
   image		=> 'ascheman/dukecon-server',
   ports		=> ['9050:8080'],
-  env		=> ['SPRING_PROFILES_ACTIVE=latest'],
+  env		=> ['SPRING_PROFILES_ACTIVE=latest,noauth'],
 }
