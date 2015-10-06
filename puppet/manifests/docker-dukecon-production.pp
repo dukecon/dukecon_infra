ef46bc7f52caab28980a6af10096a60374aa4f6f
Gerd Aschemann <gerd@aschemann.net>
2015-10-06 08:05:04 +0200

    Use special tag for production

--- puppet/manifests/docker-dukecon-production.pp
+++ puppet/manifests/docker-dukecon-production.pp
@@ -1,5 +1,5 @@
 docker::run { 'dukecon-production':
-  image		=> 'ascheman/dukecon-server',
+  image		=> 'ascheman/dukecon-server:release',
   ports		=> ['9080:8080'],
   env		=> ['SPRING_PROFILES_ACTIVE=production'],
 }
