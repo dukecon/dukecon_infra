7a22ff9126306424c875403d8a62015edd9f075e
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-12-31 11:17:04 +0100

    ensure docker module for puppet is installed

--- puppet/init-puppet-debian.sh
+++ puppet/init-puppet-debian.sh
@@ -11,5 +11,6 @@ $sudo apt-get update
 
 test -r /etc/puppet/modules/etckeeper || $sudo puppet module install thomasvandoren-etckeeper
 test -r /etc/puppet/modules/stdlib || $sudo puppet module install puppetlabs-stdlib
+test -r /etc/puppet/modules/docker|| $sudo puppet module install garethr-docker
 
 $sudo puppet apply /vagrant/puppet/manifests/debian.pp
