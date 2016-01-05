67ad91f4a941d9d88b530e504b66c90f5b763c74
Alexander Schwartz <alexander.schwartz@gmx.net>
2016-01-05 23:40:21 +0100

    Allow persistent storage for production and latest / #dukecon_server/4

--- puppet/manifests/docker-dukecon-production.pp
+++ puppet/manifests/docker-dukecon-production.pp
@@ -1,5 +1,6 @@
 docker::run { 'dukecon-production':
   image		=> 'ascheman/dukecon-server:release',
   ports		=> ['9080:8080'],
+  volumes   => ['dukecon-production:/var/cache/dukecon'],
   env		=> ['SPRING_PROFILES_ACTIVE=production'],
 }
