f5e89b31d05db11dd4a8732207924fc6cb178e78
Gerd Aschemann <gerd@aschemann.net>
2016-01-10 21:33:44 +0100

    Make sure the vagrant user has the right to run docker

--- puppet/manifests/docker-base.pp
+++ puppet/manifests/docker-base.pp
@@ -2,4 +2,12 @@ class { 'docker':
   manage_kernel 	=> false,
   # tcp_bind		=> 'tcp://0.0.0.0:2375',
   socket_bind		=> 'unix:///var/run/docker.sock',
+  # TODO: clean this up/ensure the mirror is only set in local Vagrant box
+  # extra_parameters	=> '--registry-mirror=http://10.211.55.6:5000 --insecure-registry 10.211.55.6:5000',
+}
+
+user { 'vagrant':
+  ensure => present,
+  groups => ['docker'],
+  require => Class['docker'],
 }
