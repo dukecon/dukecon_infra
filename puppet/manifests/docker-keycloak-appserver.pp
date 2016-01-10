f713be88990bfbe4a52ed4221e53ab87ab2a8e86
Gerd Aschemann <gerd@aschemann.net>
2016-01-10 21:36:08 +0100

    Added KeyCloak

---
+++ puppet/manifests/docker-keycloak-appserver.pp
@@ -0,0 +1,28 @@
+$keycloak_image = "jboss/keycloak-postgres:1.4.0.Final"
+
+# Make sure the passwords are available via Puppet Hiera, e.g., in /etc/puppet/hieradata/common.yaml
+#---
+#keycloak:
+#  postgres:
+#    password: xxx
+#	   root_password: yyy
+
+# TODO: Check if there is a better way to set these variables, e.g., by "Automatic Parameter Lookup",
+# cf. https://docs.puppetlabs.com/hiera/3.0/puppet.html#automatic-parameter-lookup
+$keycloak_hiera = hiera('keycloak')
+$keycloak_hiera_postgres = $keycloak_hiera['postgres']
+$keycloak_hiera_postgres_password = $keycloak_hiera_postgres['password']
+$keycloak_hiera_postgres_root_password = $keycloak_hiera_postgres['root_password']
+
+docker::image { $keycloak_image: }
+
+docker::run { 'keycloak':
+  image		=> $keycloak_image,
+  env			=> ['POSTGRES_DATABASE=keycloak',
+		        	'POSTGRES_USER=keycloak',
+							"POSTGRES_PASSWORD=$keycloak_hiera_postgres_password",
+						 ],
+  links   => ['postgres-keycloak:postgres',],
+  ports		=> ['9041:8080'],
+  depends	=> ['postgres-keycloak',],
+}
---
+++ puppet/manifests/docker-keycloak-database.pp
@@ -0,0 +1,33 @@
+$postgres_version = "9.3"
+$postgres_image = "postgres:$postgres_version"
+
+package { "postgresql-client-$postgres_version": }
+
+# Make sure the passwords are available via Puppet Hiera, e.g., in /etc/puppet/hieradata/common.yaml
+#---
+#keycloak:
+#  postgres:
+#    password: xxx
+#	   root_password: yyy
+
+# TODO: Check if there is a better way to set these variables, e.g., by "Automatic Parameter Lookup",
+# cf. https://docs.puppetlabs.com/hiera/3.0/puppet.html#automatic-parameter-lookup
+$keycloak_hiera = hiera('keycloak')
+$keycloak_hiera_postgres = $keycloak_hiera['postgres']
+$keycloak_hiera_postgres_password = $keycloak_hiera_postgres['password']
+$keycloak_hiera_postgres_root_password = $keycloak_hiera_postgres['root_password']
+
+docker::image { $postgres_image: }
+
+docker::run { 'postgres-keycloak':
+  image		=> $postgres_image,
+  env			=> ['POSTGRES_DATABASE=keycloak',
+		          'POSTGRES_USER=keycloak',
+		          "POSTGRES_PASSWORD=$keycloak_hiera_postgres_password",
+		          "POSTGRES_ROOT_PASSWORD=$keycloak_hiera_postgres_root_password",
+		         ],
+  ports		=> ['127.0.0.1:9432:5432'],
+  volumes	=> [# '/data/postgresql/keycloak/log:/var/log/postgresql',
+		    			'/data/postgresql/keycloak/data:/var/lib/postgresql/data',
+						 ],
+}
