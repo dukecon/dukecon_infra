aec05fc9c4dd864ecc6812d45bfe4dc61d2de546
Alexander Schwartz <alexander.schwartz@gmx.net>
2016-01-13 11:56:25 +0100

    adding settings for /etc/default/jenkins

--- puppet/init-puppet-jenkins.sh
+++ puppet/init-puppet-jenkins.sh
@@ -7,6 +7,7 @@ test -x $sudo || sudo=
 
 test -d /etc/puppet/modules/jenkins || $sudo puppet module install rtyler-jenkins
 test -d /etc/puppet/modules/maven || $sudo puppet module install maestrodev-maven
+test -d /etc/puppet/modules/stdlib || $sudo puppet module install puppetlabs-stdlib
 
 $sudo puppet apply /vagrant/puppet/manifests/jenkins.pp
 $sudo puppet apply /vagrant/puppet/manifests/jenkins-dukecon.pp
