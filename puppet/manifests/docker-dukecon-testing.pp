dd092e24eb9380106fc8c2685cd9abab4a413e67
Gerd Aschemann <gerd@aschemann.net>
2016-01-17 16:47:19 +0100

    Added Postgres DB to dukecon testing

--- puppet/manifests/docker-dukecon-testing.pp
+++ puppet/manifests/docker-dukecon-testing.pp
@@ -1,6 +1,10 @@
-docker::run { 'dukecon-testing':
-  image		=> 'ascheman/dukecon-server:latest',
-  ports		=> ['9060:8080'],
-  env		=> ['SPRING_PROFILES_ACTIVE=testing'],
-  volumes       => ['/data/dukecon/cache/dukecon-testing:/var/cache/dukecon'],
+$instance = "testing"
+
+docker::run { "dukecon-$instance":
+  image		=> "ascheman/dukecon-server:latest",
+  ports		=> ["9060:8080"],
+  env		  => ["SPRING_PROFILES_ACTIVE=$instance,postgres"],
+  volumes => ["/data/dukecon/cache/dukecon-$instance:/var/cache/dukecon"],
+  links   => ["dukecon-postgres-$instance:postgres",],
+  depends	=> ["dukecon-postgres-$instance",],
 }
