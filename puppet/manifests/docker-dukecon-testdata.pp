1e209ff2135a9a8a3c57dd5941a871caa569da3f
Gerd Aschemann <gerd@aschemann.net>
2016-01-17 12:28:20 +0100

    Switched "testdata" to port 9040

--- puppet/manifests/docker-dukecon-testdata.pp
+++ puppet/manifests/docker-dukecon-testdata.pp
@@ -1,6 +1,6 @@
 docker::run { 'dukecon-testdata':
   image		=> 'ascheman/dukecon-server:latest',
-  ports		=> ['9051:8080'],
+  ports		=> ['9040:8080'],
   env		=> ['SPRING_PROFILES_ACTIVE=testdata'],
   volumes       => ['/data/dukecon/cache/dukecon-testdata:/var/cache/dukecon'],
 }
