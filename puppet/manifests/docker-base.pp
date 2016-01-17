7a3fcd8618517452d5eae3bd2d4dcb95a7b30c5b
Gerd Aschemann <gerd@aschemann.net>
2016-01-17 16:39:42 +0100

    Add some TODOs

--- puppet/manifests/docker-base.pp
+++ puppet/manifests/docker-base.pp
@@ -1,11 +1,13 @@
 class { 'docker':
   manage_kernel 	=> false,
+  # TODO: Enable this on "development" (i.e., "local" Vagrant machine)
   # tcp_bind		=> 'tcp://0.0.0.0:2375',
   socket_bind		=> 'unix:///var/run/docker.sock',
   # TODO: clean this up/ensure the mirror is only set in local Vagrant box
   # extra_parameters	=> '--registry-mirror=http://10.211.55.6:5000 --insecure-registry 10.211.55.6:5000',
 }
 
+# TODO: Only enable this on Vagrant machine
 user { 'vagrant':
   ensure => present,
   groups => ['docker'],
