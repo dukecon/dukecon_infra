8262860e46e23fd61e752d062aac17024bc3a37c
Gerd Aschemann <gerd@aschemann.net>
2016-01-18 16:36:26 +0100

    Added workaround for postgres access + logging volume

--- puppet/manifests/docker-dukecon-testing.pp
+++ puppet/manifests/docker-dukecon-testing.pp
@@ -1,10 +1,17 @@
 $instance = "testing"
 
 docker::run { "dukecon-$instance":
-  image		=> "ascheman/dukecon-server:latest",
-  ports		=> ["9060:8080"],
-  env		  => ["SPRING_PROFILES_ACTIVE=$instance,postgres"],
-  volumes => ["/data/dukecon/cache/dukecon-$instance:/var/cache/dukecon"],
+  image   => "ascheman/dukecon-server:latest",
+  ports	  => ["9060:8080"],
+  env	  => [
+	    "SPRING_PROFILES_ACTIVE=$instance,postgresql",
+            # TODO: This is only a workaround!
+	    "DUKECON_ARGS='--postgres.host=postgres --postgres.port=5432'",
+             ],
+  volumes => [
+              "/data/dukecon/cache/dukecon-$instance:/var/cache/dukecon",
+              "/data/dukecon/logs/dukecon-$instance:/logs",
+             ],
   links   => ["dukecon-postgres-$instance:postgres",],
-  depends	=> ["dukecon-postgres-$instance",],
+  depends => ["dukecon-postgres-$instance",],
 }
