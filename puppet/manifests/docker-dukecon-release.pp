8e63b578b7e62380def52f2a6b969116378ba306
Gerd Aschemann <gerd@aschemann.net>
2016-01-24 18:44:48 +0100

    Added/Updated release instances

--- puppet/manifests/docker-dukecon-release.pp
+++ puppet/manifests/docker-dukecon-release.pp
@@ -2,13 +2,17 @@ $instance = "release"
 $port = "9070"
 
 docker::run { "dukecon-$instance":
-  image    => "ascheman/dukecon-server:release",
+  image    => "ascheman/dukecon-server:release-candidate",
   ports    => ["127.0.0.1:$port:8080"],
   env      => [
-    "SPRING_PROFILES_ACTIVE=$instance",
+    "SPRING_PROFILES_ACTIVE=$instance,postgresql,docker",
+    # TODO: This is only a workaround!
+    "DUKECON_ARGS='--postgres.host=postgres --postgres.port=5432'",
   ],
   volumes  => [
     "/data/dukecon/cache/dukecon-$instance:/var/cache/dukecon",
     "/data/dukecon/logs/dukecon-$instance:/logs",
   ],
+  links    => ["dukecon-postgres-$instance:postgres",],
+  depends  => ["dukecon-postgres-$instance",],
 }
