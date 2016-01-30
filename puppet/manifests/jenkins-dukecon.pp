5c977c2efa02921c6972eeb8a63a2418be9cb1b0
Gerd Aschemann <gerd@aschemann.net>
2016-01-30 08:44:00 +0100

    Integrated additional dukecon specific packages directly into jenkins setup

--- puppet/manifests/jenkins-dukecon.pp
+++
@@ -1,9 +0,0 @@
-# firefox is needed for GUI test of dukecon
-package { 'firefox':
-  ensure => 'latest',
-}
-
-# used for headless testing
-package { 'xvfb':
-  ensure => 'installed'
-}
--- puppet/manifests/jenkins.pp
+++ puppet/manifests/jenkins.pp
@@ -6,6 +6,16 @@ package { 'git' :
   ensure => installed,
 }
 
+# firefox is needed for GUI test of dukecon
+package { 'firefox':
+  ensure => 'latest',
+}
+
+# used for headless testing
+package { 'xvfb':
+  ensure => 'installed'
+}
+
 $plugins = [
   'git-client',
   'scm-api',
