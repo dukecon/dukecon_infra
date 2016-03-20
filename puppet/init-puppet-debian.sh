5cbc72b3f5d01c9a0a4f47bdc7d9d2a043589770
Gerd Aschemann <gerd@aschemann.net>
2016-03-20 12:31:32 +0100

    Drop one unnecessary update cycle

--- puppet/init-puppet-debian.sh
+++ puppet/init-puppet-debian.sh
@@ -7,7 +7,6 @@ test -x $sudo || sudo=
 
 $sudo apt-get update
 $sudo apt-get install -qq puppet
-$sudo apt-get update
 
 test -r /etc/puppet/modules/etckeeper || $sudo puppet module install thomasvandoren-etckeeper
 test -r /etc/puppet/modules/stdlib || $sudo puppet module install puppetlabs-stdlib
