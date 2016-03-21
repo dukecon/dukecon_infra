058915924404d14be392cb46b13bda0f05b4889c
Gerd Aschemann <gerd@aschemann.net>
2016-03-21 12:07:43 +0100

    Docker is provided by init-puppet-docker-base!

--- puppet/init-puppet-debian.sh
+++ puppet/init-puppet-debian.sh
@@ -12,6 +12,5 @@ $sudo apt-get install -qq puppet
 
 test -r /etc/puppet/modules/etckeeper || $sudo puppet module install thomasvandoren-etckeeper
 test -r /etc/puppet/modules/stdlib || $sudo puppet module install puppetlabs-stdlib
-test -r /etc/puppet/modules/docker|| $sudo puppet module install garethr-docker
 
 $sudo puppet apply $dir/puppet/manifests/debian.pp
