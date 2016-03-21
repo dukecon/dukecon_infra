d144396df27c2394ac6fbd5f1f06308b1b90c454
Gerd Aschemann <gerd@aschemann.net>
2016-03-21 20:18:16 +0100

    Misc. improvements for Oracle Java 8 install
    - Only add oracle java 8 if it is not yet installed
    - Add/update gpg keys
    - Force installation (overrides unknown keys, TODO: remove when keys are finally added)

--- init-java8-on-trusty.sh
+++ init-java8-on-trusty.sh
@@ -5,14 +5,17 @@ set -e
 sudo=/usr/bin/sudo
 test -x $sudo || sudo=
 
-$sudo apt-get -y -q update
-$sudo apt-get -y -q upgrade
-$sudo apt-get -y -q install software-properties-common htop
-if ! test -r /etc/apt/sources.list.d/webupd8team-java-trusty.list; then
-	$sudo add-apt-repository ppa:webupd8team/java
-	$sudo apt-get -y -q update
-	$sudo etckeeper commit 'Added Oracle Java 8 repository'
+if ! test -d /usr/lib/jvm/java-8-oracle; then
+    $sudo apt-get -y -q install software-properties-common htop
+    if ! test -r /etc/apt/sources.list.d/webupd8team-java-trusty.list; then
+        $sudo add-apt-repository ppa:webupd8team/java
+        $sudo apt-get -y -q update
+        $sudo etckeeper commit 'Added Oracle Java 8 repository'
+    fi
+    $sudo apt-get update
+    $sudo apt-key update
+
+    echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | $sudo /usr/bin/debconf-set-selections
+    $sudo apt-get --force-yes -y -q install oracle-java8-installer
 fi
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | $sudo /usr/bin/debconf-set-selections
-$sudo apt-get -y -q install oracle-java8-installer
 $sudo update-java-alternatives -s java-8-oracle
