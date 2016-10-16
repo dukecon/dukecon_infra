9f7e934de371b56071e9f5e48cb908669ce98ed9
Gerd Aschemann <gerd@aschemann.net>
2016-10-16 15:51:40 +0200

    Add private "sched" IDs to configuration

--- puppet/manifests/docker-dukecon-latest.pp
+++ puppet/manifests/docker-dukecon-latest.pp
@@ -5,10 +5,11 @@ docker::run { "dukecon-$instance":
   image    => "ascheman/dukecon-server:latest",
   ports    => ["127.0.0.1:$port:8080"],
   env      => [
-    "SPRING_PROFILES_ACTIVE=$instance,docker",
+    "SPRING_PROFILES_ACTIVE=$instance,docker,private-sched-ids",
   ],
   volumes  => [
     "/data/dukecon/cache/dukecon-$instance:/var/cache/dukecon",
     "/data/dukecon/logs/dukecon-$instance:/logs",
+    "/data/dukecon/config-common:/opt/dukecon/config",
   ],
 }
