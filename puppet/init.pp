ab776b494d6c4e04bbcfae0b44f849643ce64669
Gerd Aschemann <gerd@aschemann.net>
2017-06-25 18:00:03 +0200

    Start InfluxDB (for inspectIT)

---
+++ puppet/init.pp
@@ -0,0 +1,14 @@
+docker::image { 'influxdb': }
+
+file { '/data/influxdb':
+  ensure   => directory,
+  owner    => root,
+  group    => root,
+  mode     => 0755,
+}
+->
+docker::run { 'influxdb':
+  image    => 'influxdb',
+  volumes  => ['/data/influxdb:/var/lib/influxdb'],
+  ports    => ['127.0.0.1:8086:8086'],
+}
