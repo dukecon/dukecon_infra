4bc6202159b2b37d1b1e2b3559fbdf381a70551a
Gerd Aschemann <gerd@aschemann.net>
2017-07-08 18:01:10 +0200

    Fix Puppet modules to certain versions

--- puppet/init-puppet-debian.sh
+++ puppet/init-puppet-debian.sh
@@ -15,7 +15,8 @@ $sudo apt-get update
 $sudo apt-get install -qq puppet
 
 test -r /etc/puppet/modules/etckeeper || $sudo puppet module install thomasvandoren-etckeeper
-test -r /etc/puppet/modules/stdlib || $sudo puppet module install puppetlabs-stdlib
+test -r /etc/puppet/modules/stdlib || $sudo puppet module install puppetlabs-stdlib --version 4.12.0
+test -r /etc/puppet/modules/apt || $sudo puppet module install --force puppetlabs-apt --version 2.3.0
 test -r /etc/puppet/modules/inifile || $sudo puppet module install puppetlabs-inifile
 
 $sudo puppet apply $dir/puppet/manifests/debian.pp
