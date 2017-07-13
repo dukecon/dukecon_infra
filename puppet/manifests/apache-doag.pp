6186a4f5d68877a49ebb43389fcf7647e7980715
Gerd Aschemann <gerd@aschemann.net>
2017-07-14 00:47:19 +0200

    Apache DOAG: Fix indentation and use dukecon-unstable instead of latest

--- puppet/manifests/apache-doag.pp
+++ puppet/manifests/apache-doag.pp
@@ -3,29 +3,27 @@ class { 'apache':
   default_vhost => false,
 }
 
-  
-  apache::vhost { 'programm1.doag.org':
-    servername            => 'programm1.doag.org',
-    ip                    => '85.214.231.45',
-    port                  => '443',
-    ssl                   => true,
-    ssl_cert              => '/etc/tls/doag2015.crt',
-    ssl_key               => '/etc/tls/doag2014.key',
-    ssl_ca                => '/etc/tls/intermediate2015.crt',
-    docroot               => '/var/www/html',
-    allow_encoded_slashes => 'nodecode',
-    # add "X-Forwarded-Proto: https" to all forwarded requests on this SSL port
-    request_headers       => [ 'set X-Forwarded-Proto https' ],
-    proxy_preserve_host   => 'true',
-    proxy_pass_match      => [
-      { 'path' => '^/(\w+)/(\d+)/rest/init.json',
-        'url'  => 'http://localhost:9050/latest/rest/init/$1/$2',
-      },
-      { 'path'  =>  '^/(\w+)/(\d+)/(.*)',
-        'url'   =>  'http://localhost:9050/latest/$3',
-      },
-    ],
-    # http://stackoverflow.com/questions/32120129/keycloak-is-causing-ie-to-have-an-infinite-loop
-    headers               => 'set P3P "CP=\"Potato\""'
-  }
- 
+apache::vhost { 'programm1.doag.org':
+  servername            => 'programm1.doag.org',
+  ip                    => '85.214.231.45',
+  port                  => '443',
+  ssl                   => true,
+  ssl_cert              => '/etc/tls/doag2015.crt',
+  ssl_key               => '/etc/tls/doag2014.key',
+  ssl_ca                => '/etc/tls/intermediate2015.crt',
+  docroot               => '/var/www/html',
+  allow_encoded_slashes => 'nodecode',
+  # add "X-Forwarded-Proto: https" to all forwarded requests on this SSL port
+  request_headers       => [ 'set X-Forwarded-Proto https' ],
+  proxy_preserve_host   => 'true',
+  proxy_pass_match      => [
+    { 'path' => '^/(\w+)/(\d+)/rest/init.json',
+      'url'  => 'http://localhost:9051/develop/rest/init/$1/$2',
+    },
+    { 'path'  =>  '^/(\w+)/(\d+)/(.*)',
+      'url'   =>  'http://localhost:9051/develop/$3',
+    },
+  ],
+  # http://stackoverflow.com/questions/32120129/keycloak-is-causing-ie-to-have-an-infinite-loop
+  headers               => 'set P3P "CP=\"Potato\""'
+}
