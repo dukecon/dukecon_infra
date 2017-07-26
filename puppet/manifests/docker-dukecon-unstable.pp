faaa3cc97009037ebc9979c9a7fcedb5603ad9bd
Gerd <gerd@aschemann.net>
2017-07-26 22:20:12 +0200

    Add config location to dukecon-unstable

--- puppet/manifests/docker-dukecon-unstable.pp
+++ puppet/manifests/docker-dukecon-unstable.pp
@@ -6,9 +6,11 @@ docker::run { "dukecon-$instance":
   ports    => ["127.0.0.1:$port:8080"],
   env      => [
     "SPRING_PROFILES_ACTIVE=$instance,docker",
+    "SPRING_CONFIG_LOCATION=/opt/dukecon/config",
   ],
   volumes  => [
     "/data/dukecon/cache/dukecon-$instance:/var/cache/dukecon",
     "/data/dukecon/logs/dukecon-$instance:/opt/dukecon/logs",
+    "/data/dukecon/config/dukecon-$instance:/opt/dukecon/config",
   ],
 }
