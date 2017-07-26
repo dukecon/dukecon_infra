cc9e3992dbdaad3741e05fac751ea1409214653b
Gerd Aschemann <gerd@aschemann.net>
2017-07-27 00:39:48 +0200

    Setup/re-configure dukecon jfs data dirs

--- puppet/manifests/docker-dukecon-jfs.pp
+++ puppet/manifests/docker-dukecon-jfs.pp
@@ -1,18 +1,50 @@
 $instance="jfs"
 $port = "9052"
+$image = "dukecon/dukecon-server:1.4-SNAPSHOT"
 
+file { "/data":
+  ensure        =>      directory,
+  mode          =>      '0755',
+}
+->
+file { "/data/dukecon":
+  ensure        =>      directory,
+  mode          =>      '0755',
+}
+->
+file { "/data/dukecon/$instance":
+  ensure        =>      directory,
+  mode          =>      '0755',
+}
+->
+file { "/data/dukecon/$instance/config":
+  ensure        =>      directory,
+  mode          =>      '0755',
+}
+->
+file { "/data/dukecon/$instance/cache":
+  ensure        =>      directory,
+  mode          =>      '0755',
+}
+->
+file { "/data/dukecon/$instance/logs":
+  ensure        =>      directory,
+  mode          =>      '0755',
+}
+->
 docker::run { "dukecon-$instance":
-  image    => "dukecon/dukecon-server:1.4-SNAPSHOT",
+  image    => $image,
   ports    => ["127.0.0.1:$port:8080"],
   env      => [
     "SPRING_PROFILES_ACTIVE=$instance,docker",
+    "SPRING_CONFIG_LOCATION=/opt/dukecon/config",
     "JAVA_OPTS='-javaagent:/opt/inspectit/agent/inspectit-agent.jar -Dinspectit.repository=inspectit:9070 -Dinspectit.agent.name=DukeCon-$instance'",
   ],
   links    => ["inspectit"],
   volumes  => [
-    "/data/dukecon/cache/dukecon-$instance:/var/cache/dukecon",
-    "/data/dukecon/logs/dukecon-$instance:/opt/dukecon/logs",
-    "/data/dukecon/config-common:/opt/dukecon/config",
+    "/data/dukecon/$instance/cache:/opt/dukecon/cache",
+    "/data/dukecon/$instance/config:/opt/dukecon/config",
+    "/data/dukecon/$instance/logs:/opt/dukecon/logs",
     "/opt/inspectit/agent:/opt/inspectit/agent",
   ],
 }
