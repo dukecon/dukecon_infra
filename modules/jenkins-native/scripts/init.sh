8036a07e905b3592717e3b543a9f726f37f021f0
Gerd Aschemann <gerd@aschemann.net>
2018-01-02 11:19:23 +0100

    Drop installation of puppetlabs-stdlib from Jenkins
    since it is already part of the base Puppet installation

--- modules/jenkins-native/scripts/init.sh
+++ modules/jenkins-native/scripts/init.sh
@@ -20,7 +20,6 @@ test -x $sudo || sudo=
 
 test -d /etc/puppet/modules/maven || $sudo puppet module install maestrodev-maven
 test -d /etc/puppet/modules/jenkins || $sudo puppet module install rtyler-jenkins
-test -d /etc/puppet/modules/stdlib || $sudo puppet module install puppetlabs-stdlib
 
 # This is the minimum DukeCon Docker application which is needed for Jenkins
 $sudo puppet apply ${dir}/../../../puppet/manifests/docker-dukecon-latest.pp
