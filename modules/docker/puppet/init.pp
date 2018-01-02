570b527c379195a3b2707a1698fcba4054cf8611
Gerd Aschemann <gerd@aschemann.net>
2018-01-02 07:10:28 +0100

    Fix indentation

--- modules/docker/puppet/init.pp
+++ modules/docker/puppet/init.pp
@@ -5,24 +5,24 @@ $hiera_docker_registry_mirror = $hiera_docker_registry['mirror']
 class { 'docker':
   manage_kernel 	=> false,
   # TODO: Enable this on "development" (i.e., "local" Vagrant machine)
-  tcp_bind		=> 'tcp://127.0.0.1:2375',
-  socket_bind		=> 'unix:///var/run/docker.sock',
+  tcp_bind         => 'tcp://127.0.0.1:2375',
+  socket_bind	   => 'unix:///var/run/docker.sock',
   # TODO: clean this up/ensure the mirror is only set in local Vagrant box
   extra_parameters => "--registry-mirror=http://$hiera_docker_registry_mirror --insecure-registry $hiera_docker_registry_mirror",
 }
 
 file { "/data":
-  path          =>      "/data",
-  ensure        =>      directory,
-  mode          =>      '0755',
+  path             =>      "/data",
+  ensure           =>      directory,
+  mode             =>      '0755',
 }
 
 exec { '/bin/echo "This is a Vagrant Host"':
-  onlyif        => '/bin/grep -q vagrant /etc/passwd',
+  onlyif           => '/bin/grep -q vagrant /etc/passwd',
 }
 ->
 user { 'vagrant':
-  ensure        => present,
-  groups        => ['docker'],
-  require       => Class['docker'],
+  ensure           => present,
+  groups           => ['docker'],
+  require          => Class['docker'],
 }
