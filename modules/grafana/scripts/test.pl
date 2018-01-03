b11c4602284e3b840751111d79c8ae57a21a3c3f
Gerd Aschemann <gerd@aschemann.net>
2018-01-03 23:34:11 +0100

    Fix test for Grafana

--- modules/grafana/scripts/test.pl
+++ modules/grafana/scripts/test.pl
@@ -8,7 +8,7 @@ use Test::Simple tests => 1;
 
 # Create a new mechanize object
 my $mech = WWW::Mechanize->new();
-my $url = 'http://localhost:3000/';
+my $url = 'http://localhost:3000/login';
 # Associate the mechanize object with a URL
 $mech->get($url);
 # Test for the logo in the content of the URL
