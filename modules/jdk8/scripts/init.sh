993b0d1182edd8c5020cbd15376664f9ae1067bd
Gerd Aschemann <gerd@aschemann.net>
2018-01-03 11:38:20 +0100

    Drop Oracle JDK 8 test since it is optional now

--- modules/jdk8/scripts/init.sh
+++ modules/jdk8/scripts/init.sh
@@ -26,5 +26,3 @@ test -d /etc/puppetlabs/code/environments/production/modules/oraclejdk8 || $sudo
 $sudo /opt/puppetlabs/bin/puppet apply ${basedir}/puppet/init.pp
 # For some reason the first attempt fails very often if not always - so give it a second try
 test -d /usr/lib/jvm/java-8-oracle || $sudo /opt/puppetlabs/bin/puppet apply ${basedir}/puppet/init.pp
-
-$dir/test.sh
