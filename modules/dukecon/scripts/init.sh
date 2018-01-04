ddf41199d401b67a21968bce4be3d0db1b0d1f45
Gerd Aschemann <gerd@aschemann.net>
2018-01-04 09:54:33 +0100

    Migrate DukeCon (Testing) setup to a "regular" module

---
+++ modules/dukecon/scripts/init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eu
+
+dir=`dirname $0`
+
+basedir="${dir}/.."
+
+modulename=`dirname $basedir`
+modulename=`dirname $modulename`
+modulename=`basename $modulename`
+cat<<EOM
+======================================================================
+Installing "${modulename}"
+======================================================================
+EOM
+
+sudo=/usr/bin/sudo
+test -x $sudo || sudo=
+
+#test -x /usr/local/bin/docker-compose || \
+#  $sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+#export
+
+$sudo FACTER_module_basedir=`realpath "${basedir}"` puppet apply ${basedir}/puppet/init.pp
